218405c9149741241ffddbbcbfa93026a7fab1ca
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-18 15:25:57 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+<h1>HEADER</h1>
+</template>
+
+<style lang="scss" scoped>
+h1{
+    color: black;
+}
+</style>
